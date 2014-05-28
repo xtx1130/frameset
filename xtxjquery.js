51f03297c186e59f0644a72f48f5ab5d9ecafa11
xtx <xtx1130@gmail.com>
2014-05-28 16:44:39 +0800

    xtxs jquery formhandler

--- xtxjquery.js
+++ xtxjquery.js
@@ -18,25 +18,29 @@
 			LOAD_ARR: [],
 			/**
 			 *@description 开辟子命名空间，支持开辟多级空间
-			*/
+			 */
 			namespace: function(name) {
 				if (typeof name != 'string') {
 					return false;
 				}
 				name = name.split('.');
-				var o = this,
-					n;
-				for (var i = 0, len = name.length; i < len, n = name[i]; i++) {
-					o = (o[n] = o[n] || {});
+				var that = this,
+					key;
+				for (var i = 0, len = name.length; i < len, key = name[i]; i++) {
+					that = (that[key] = that[key] || {});
 				}
-				return o;
+				return that;
 			},
 			/**
 			 *@description 动态加载js方法，可加载多个js
-			 *@param src [string||array] 需要动态加载的js
-			 *@param callback [function] 加载完成的回调
+			 *@param src {string||array} 需要动态加载的js
+			 *@param callback {function} 加载完成的回调
 			 */
 			load: function(src, callback) {
+				var baseobj = {},
+					fsobj = {},
+					arr = [];
+
 				function load(s) {
 					xtx.LOAD_ARR.push(arr[s]);
 					if (s == arr.length - 1) {
@@ -66,19 +70,18 @@
 						callback.call()
 					});
 				} else if (Object.prototype.toString.call(src) == '[object Array]') {
-					var o = {}, o1 = {}, arr = [];
 					for (var i = 0; i < xtx.LOAD_ARR.length; i++) {
-						o[xtx.LOAD_ARR[i]] = i + 1;
-						o1[xtx.LOAD_ARR[i]] = 1;
+						baseobj[xtx.LOAD_ARR[i]] = i + 1;
+						fsobj[xtx.LOAD_ARR[i]] = 1;
 					}
 					for (var j = 0; j < src.length; j++) {
-						if (!o[src[j]]) {
-							o[src[j]] = i + 1;
+						if (!baseobj[src[j]]) {
+							baseobj[src[j]] = i + 1;
 							i++;
 						}
 					}
-					for (var k in o) {
-						if (!o1[k]) {
+					for (var k in baseobj) {
+						if (!fsobj[k]) {
 							arr.push(k);
 						}
 					}
@@ -87,7 +90,7 @@
 			},
 			/**
 			 *@description 提供reg匹配方法，表单中会用到
-			*/
+			 */
 			reg: {
 				rUrl: function(url) {
 					var regExp = /(?:(?:http[s]?|ftp):\/\/)?[^\/\.]+?(\.)?[^\.\\\/]+?\.\w{2,}.*$/i;
@@ -152,5 +155,166 @@
 			}
 		};
 	}());
+	//view 相关东西beginning,内容中绑定了界面元素
+	xtx.namespace('view');
+	xtx.view.formHanderConfig = {
+		commitstate:'status',
+		ajaxstate:'status',
+		ajaxtype:'post'
+	}
+	xtx.view.formHandler =function(id) {
+		/**
+		 *@description form提交控制函数，
+		 class：submit是提交，
+		 reset是重置，
+		 back返回上一个页面
+		 on 值不能为空
+		 *@param id {string} 绑定的form的id
+	     *@param {function} ajax完成后的回调
+		 *@param {function} 提交之前的回调
+		 *@author tianxin@leju.com
+		*/
+		var that = $(id),
+			commit = that.find('.submit'),
+			reset = that.find('.reset'),
+			back = that.find('.back'),
+			input = that.find('input'),
+			action = that.attr('action'),
+			re = /(rNumber)|(rUrl)|(rWeibo)|(rEmail)|(rPhoneNumber)|(rQQ)|(rEnglish)/ig,
+			option = arguments,
+			method = $(that).attr('method') || 'post',
+			o = {};
+
+		function doCheckErrortext(obj,str){
+			if(obj.next('.error_text').length){
+				obj.next('.error_text').html(str);
+			}else{
+				that.find('.error_text').html(str);
+			}
+		}
+
+		function doCheckNull() {
+			var text = that.find(".on"),
+				length = text.length;
+			while (--length >= 0) {
+				if (text.eq(length).val() == '') {
+					text.eq(length).addClass('error');
+					doCheckErrortext(text.eq(length),'此项不能为空！');
+				} else {
+					text.eq(length).removeClass('error');
+					doCheckErrortext(text.eq(length),'');
+				}
+			}
+		}
+
+		function doCheckReg(_this, cla) {
+			//正则验证
+			if (xtx.reg[cla](String(_this.val()))) {
+				doCheckErrortext(_this,'');
+				_this.removeClass('error');
+			} else {
+				doCheckErrortext(_this,'您书写的格式不符合规范！');
+				_this.addClass('error');
+			}
+		}
+
+		function ajaxConform(obj) {
+			var url = obj.attr('url_check'),
+				d = obj.attr('data').split(' '),
+				o = {},
+				dstr = [];
+			for (var i = 0; i < d.length; i++) {
+				dstr[i] = d[i].split(':');
+				if (dstr[i][1]) {
+					o[dstr[i][0]] = dstr[i][1];
+				} else {
+					o[dstr[i][0]] = obj.val();
+				}
+			}
+			$.ajax({
+				url: url,
+				type: xtx.view.formHanderConfig.ajaxtype,
+				dataType: "json",
+				data: o,
+				success: function(result) {
+					if (typeof result != 'object')
+						result = $.parseJSON(result);
+					if (!result[xtx.view.formHanderConfig.ajaxstate]) {
+						obj.addClass('error');
+						var str = result.msg;
+						doCheckErrortext(obj,str);
+					} else {
+						obj.removeClass('error');
+						doCheckErrortext(obj,'');
+					}
+				}
+			});
+		}
+
+		$.merge(input, that.find('select'));
+		if (input) {
+			input.each(function() {
+				if ($(this).attr('class')) {
+					var cla = String($(this).attr('class').match(re));
+					if (cla != 'null') {
+						$(this).on('blur', function() {
+							doCheckReg($(this), cla);
+						});
+					}
+				}
+			})
+		}
+		if (that.find('[url_check]')) {
+			that.find('[url_check]').each(function() {
+				$(this).on('blur', function() {
+					ajaxConform($(this));
+				});
+			})
+		}
+		back.on('click', function(e) {
+			e.preventDefault();
+			window.history.back();
+		});
+		commit.on('click', function(e) {
+			e.preventDefault();
+			doCheckNull();
+			if (option[2]) {
+				option[2].call(that)
+			}
+			if (!that.find('.error').length) {
+				if (that.hasClass('Ajax')) {
+					for (var i = 0; i < input.length; i++) {
+						o[input.eq(i).attr('name')] = input.eq(i).val();
+					}
+					$.ajax({
+						type: method,
+						url: action,
+						data: o,
+						success: function(opt) {
+							if (typeof opt != 'object')
+								opt = $.parseJSON(opt);
+							if (!opt[xtx.view.formHanderConfig.commitstate]) {
+								alert(opt.error);
+							} else {
+								if (option[1]) {
+									option[1].call(that, opt);
+								}
+							}
+						}
+					});
+				} else {
+					that.submit();
+				}
+			} else {
+				return false;
+			}
+		});
+		reset.on('click', function(e) {
+			e.preventDefault();
+			that.find("input[type='text']").each(function() {
+				$(this).val('');
+			});
+		});
+	} //formhander end
 	_.xtx = xtx;
 })(jQuery, window);
