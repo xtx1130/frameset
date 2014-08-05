6a489aa2dab0699d9abdfb582272ee7903bccbf8
xtx1130 <xtx1130@gmail.com>
2014-08-05 15:49:48 +0800

    AMD added

--- AMD/test.js
+++
@@ -1,5 +0,0 @@
-define(['test'],function(){
-	console.log('test');
-	xtx.test=function(){console.log('this is a test')};
-	return 2;
-});
--- AMD/test1.js
+++
@@ -1,5 +0,0 @@
-define(['test1'],function(){
-	console.log('test1')
-	//xtx.test=function(){console.log('this is a test')};
-	return 3;
-});
